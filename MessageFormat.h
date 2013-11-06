e29f4ff705be22f5ddae45383b209fbdacfaf5be
sandeshks <sandesh.ks1@gmail.com>
2013-11-06 00:47:42 -0600

    name change

--- MessageFormat.h
+++ MessageFormat.h
@@ -4,12 +4,12 @@
 #pragma once
 
 
-	struct AlgoMsg{
+	typedef struct AlgoMsg{
 		int TYPE; // 0: request_token, 1: have-token, 2: release, 3:send-token
 		int ORIGIN;
 		long SEQ;
 		int sender;
-	}
+	}messagetype;
 
 
 #endif
